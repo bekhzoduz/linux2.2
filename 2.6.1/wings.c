58bd4e5a52a2b1a577b1562fdf0fc2ee54ac79ec
bekhzoduz <isomidinovbekzod@gmail.com>
2024-11-20 13:04:59 +0500

    fix

--- 2.6.1/wings.c
+++ 2.6.1/wings.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <limits.h>
 
 int main() {
-    char actual_path[PATH_MAX];
-    const char *symlink_path = "./wings";
 
-    // Symlinkdan haqiqiy yo'lni olish
-    if (realpath(symlink_path, actual_path) != NULL) {
-        printf("%s\n", actual_path);
-    } else {
-        perror("Error: ");
-    }
 
     return 0;
 }
